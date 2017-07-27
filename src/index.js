2b699391188197c79e7266daee57af83f8067ed5
Douglas Soule <souleman13@gmail.com>
2017-07-27 11:59:30 -0600

    material-ui setup
    created theme and theme provider, now displays the appbar header

--- src/index.js
+++ src/index.js
@@ -4,8 +4,14 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import TouchTapPlugin from 'react-tap-event-plugin'
 
+import theme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
 const Wrapped = (
-    <App/>
+    <MuiThemeProvider muiTheme={getMuiTheme(theme)}>
+        <App/>
+    </MuiThemeProvider>
 );
 
 TouchTapPlugin();
