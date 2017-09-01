a5db3f4dcef34044ac777592286430ef971f8c17
Douglas Soule <souleman13@gmail.com>
2017-09-01 02:57:33 -0600

    push

--- src/config/GraphQL/enums.js
+++ src/config/GraphQL/enums.js
@@ -1,3 +1,30 @@
 /**
  * Created by Douglas on 8/30/2017.
  */
+export const Kind_Of_Venue = [
+    'Bar',
+    'Restaurant',
+    'ConcertHall',
+    'Club',
+    'Park',
+    'Lounge',
+    'Brewery',
+    'Gastropub',
+    'EventSpace',
+]
+
+export const Kind_Of_Event = [
+    'DJ',
+    'LiveBand',
+    'Theater',
+    'Festival',
+    'Comedy',
+    'Sports',
+    'eSports',
+    'Dancing',
+    'Karaoke',
+    'DrinkSpecial',
+    'HappyHour',
+
+
+]
--- src/config/GraphQL/query.js
+++ src/config/GraphQL/query.js
@@ -1,3 +1,41 @@
 /**
  * Created by Douglas on 8/29/2017.
  */
+import {gql} from 'react-apollo'
+
+export const getUser = gql`
+    query ($id:ID){
+        User(id:$id){
+            firstName
+            lastName
+            email
+            zip
+            venue{
+                id
+                address
+                city
+                state
+                zip
+                name
+                kind
+                ageLimit
+                alcohol
+                url
+                phone
+                events{
+                    id
+                    name
+                    kind
+                    date
+                }
+            }}}`
+
+export const getIDs = gql`
+    query ($id:ID){
+        User(id:$id){
+            venue{
+                id
+                events{
+                    id
+                }
+            }}}`
